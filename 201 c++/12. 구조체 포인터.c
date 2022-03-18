fd402aa13facbd5e25020b02efbbdc332f6aa791
정현진 <s2113@e-mirim.hs.kr>
2022-03-18 10:20:35 +0900

    설명 주석 추가

--- 201 c++/12. 구조체 포인터.c
+++ 201 c++/12. 구조체 포인터.c
@@ -5,7 +5,7 @@ typedef struct {
 	float y;
 }Point;
 
-void AddValueToPoint(Point p);
+void AddValueToPoint(Point* p);
 
 int main(void)
 {
@@ -13,13 +13,13 @@ int main(void)
 	point.x = 5.0f; 
 	point.y = 10.0f;
 	//call by value라서 값이 안바뀜
-	AddValueToPoint(point);
+	AddValueToPoint(&point); //argument
 	printf("%1.f, %1.f\n", point.x, point.y);
 	return 0;
 }
 
-void AddValueToPoint(Point p)
+void AddValueToPoint(Point* p) //parameter
 {
-	p.x += 10.0f;
-	p.y += 15.0f;
+	p->x += 10.0f; //. 대신 ->로 표시
+	p->y += 15.0f;
 }
