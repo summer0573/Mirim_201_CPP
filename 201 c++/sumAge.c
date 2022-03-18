a571b1314e9036f885376929dee3eea213cae688
정현진 <s2113@e-mirim.hs.kr>
2022-03-18 12:15:17 +0900

    배열연습3

--- 201 c++/sumAge.c
+++ 201 c++/sumAge.c
@@ -6,22 +6,19 @@ int main(void) {
 	int array[10];
 	int sum = 0;
 	double avg = 0;
+	srand(time(NULL));
 
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < sizeof(array)/sizeof(int); i++) {
 		array[i] = (rand() % 100) + 1; //°ª ÀúÀå
-	}
-
-	for (int i = 0; i < 10; i++) {
 		printf("%d ", array[i]);
-	}
-
-	for (int i = 0; i < 10; i++) {
 		sum += array[i]; //ÇÕ
 	}
 
 	avg = (double) sum / 10; //Æò±Õ
 
-	printf("\nÇÕ : %d, Æò±Õ : %1.f", sum, avg);
+	printf("\n");
+	printf("ÇÕ : % d\n", sum);
+	printf("Æò±Õ : %lf", avg);
 
 	return 0;
 }
