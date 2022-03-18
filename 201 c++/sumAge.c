6c7db97b5cff983a4beb91a87e6b9798fd4abd04
정현진 <s2113@e-mirim.hs.kr>
2022-03-18 12:14:50 +0900

    배열 연습2

--- 201 c++/sumAge.c
+++ 201 c++/sumAge.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 
 int main(void) {
 	int array[10];
 	int sum = 0;
 	double avg = 0;
 
-	for (int i = 0; i <= 10; i++) {
-		array[i] = (rand() % 100 + 1); //값 저장
-		sum += array[i]; //합
+	for (int i = 0; i < 10; i++) {
+		array[i] = (rand() % 100) + 1; //값 저장
+	}
+
+	for (int i = 0; i < 10; i++) {
 		printf("%d ", array[i]);
 	}
-	avg = sum / 10; //배열
+
+	for (int i = 0; i < 10; i++) {
+		sum += array[i]; //합
+	}
+
+	avg = (double) sum / 10; //평균
 
 	printf("\n합 : %d, 평균 : %1.f", sum, avg);
 
