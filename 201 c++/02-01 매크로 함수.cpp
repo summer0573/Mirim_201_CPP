b1a1350aeba92bf064635d8ae37970b2cc16ce85
정현진 <s2113@e-mirim.hs.kr>
2022-04-01 10:08:10 +0900

    매크로 함수 완성

--- 201 c++/02-01 매크로 함수.cpp
+++ 201 c++/02-01 매크로 함수.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
 
-#define jegob(A) A*A
+#define jegob(A) ((A)*(A))
 
 int main(void) {
-	cout << jegob(3 + 1) << endl;
-	cout << 3 + 1 * 3 + 1 << endl;
+	//cout << jegob(3 + 1) << endl; //4*4가 아닌 의도치 않은 결과
+	//cout << (3 + 1) * (3 + 1) << endl; //위에거와 같음
+
+	cout << jegob(3 + 1) << endl; //4*4가 아닌 의도치 않은 결과
+	cout << ((3 + 1) * (3 + 1)) << endl; //위에거와 같음
 }
