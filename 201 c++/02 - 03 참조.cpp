0f9974252f19804bc48984322442802121825eee
정현진 <s2113@e-mirim.hs.kr>
2022-04-05 10:56:26 +0900

    참조와 포인터

---
+++ 201 c++/02 - 03 참조.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	int a;
+	int* pa = &a;
+	*pa = 30; //a값이 30으로 바뀜
+	cout << a << endl;
+
+	//참조로 a값을 바꿈
+	int& ra = a;
+	ra = 50; //a값이 50으로 바뀜
+	cout << a << endl;
+}
