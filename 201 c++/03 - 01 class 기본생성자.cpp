5a98d587f87b50f9b434bbf3c4e85ff5bac36e93
정현진 <s2113@e-mirim.hs.kr>
2022-04-19 11:55:05 +0900

    생성자

--- 201 c++/03 - 01 class 기본생성자.cpp
+++ 201 c++/03 - 01 class 기본생성자.cpp
@@ -5,14 +5,20 @@ class Student {
 private:
 	int nHakbun;
 	string sName;
+
+	//생성자: 객체가 생성되면 자동으로 호출되는 함수
+	//반환형을 갖고있지 않는다.
+public:
+	Student() {
+		nHakbun = 1234;
+		sName = "이사랑";
+		cout << "학번이 등록되었습니다." << endl;
+	}
 	//아래와 같은 생서자를 컴파일러가 만들어줌
 	//Student() {}
 };
 
 int main(void) {
-	//기본 생성자
-	Student student1;
-	Student Student2 = Student();
-
+	Student stu;
 	return 0;
 }
