3f3a71fb5c78b7726e661b715d2662ce5bff873d
정현진 <s2113@e-mirim.hs.kr>
2022-05-17 11:18:48 +0900

    string 대신에 전통적인 방식(char*)으로 생성자 변형

--- 201 c++/03 - 01 class 기본생성자.cpp
+++ 201 c++/03 - 01 class 기본생성자.cpp
@@ -1,32 +1,33 @@
 #include <iostream> //cout, endl
+#include <string.h> //strcpy() 사용
 using namespace std;
 
 
 class Student {
 private:
 	int nHakbun;
-	string sName;
+	const char* sName;
 
 	//생성자: 객체가 생성되면 자동으로 호출되는 함수
 	//반환형을 갖고있지 않는다.
 public:
 	Student();
-	Student(int Hakbun, string eName);
+	Student(int Hakbun, const char* eName);
 	void show();
 };
 
-	Student::Student(void)
+	Student::Student()
 	{
-		this->nHakbun = 1234;
-		this->sName = "이사랑";
-		cout << "학번이 등록되었습니다." << endl;
 	}
-	Student::Student(int Hakbun, string Name)
+	Student::Student(int Hakbun, const char* Name)
 		// 객체생성과 동시에 멤버변수 초기화 // 멤버변수(매개변수)
 		// const/참조형 멤버변수를 사용할 수 있다.
-		: nHakbun(Hakbun), sName(Name)
+		: nHakbun(Hakbun)
 	{
-		cout << "학번이 등록되었습니다." << endl;
+		cout << "일반생성자 호출" << endl;
+		int len = strlen(Name) + 1;
+		sName = new char[len];
+		strcpy(sName, Name);
 	}
 	void Student::show(void)
 	{
@@ -37,12 +38,7 @@ public:
 
 int main(void) {
 
-	Student* stu2 = new Student[6];
-	for (int i = 0; i < 6; i++) {
-		stu2[i].show();
-	}
-
-	delete []stu2;
-
+	Student stu1 = Student(2111, "JHJ");
+	stu1.show();
 	return 0;
 }
