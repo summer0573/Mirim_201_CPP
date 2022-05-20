30380890f2ab010730477c82bf26d2e83074ce8c
정현진 <s2113@e-mirim.hs.kr>
2022-05-20 10:19:06 +0900

    오류 고침

--- 201 c++/20220520 코드 수정하기.cpp
+++ 201 c++/20220520 코드 수정하기.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 
 class CBox {
-private:
+//private:
+//	double width;
+
+public:
 	double width;
 };
 
@@ -13,3 +16,11 @@ public :
 		width = w;
 	}
 };
+
+/*
+private : 다 - 클래스 내부에서만 접근 가능하며 클래스 외부에서는 접근할 수 없으므로 은닉할 정보에 붙인다.
+클래스를 생성할 때 디폴트로 지정 된다.
+protected : 나 - 클래스의 내부, 상속 관계에 있는 클래스에서만 접근할 수 있다.
+public  : 가 - 어디에서든 자유롭게 참조할 수 있으며 외부에서 직접 멤버를 조작할 수 있으므로 은닉할 정보에
+붙이면 안된다.
+*/
