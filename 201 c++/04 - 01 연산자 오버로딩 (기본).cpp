0a741665db9448543539689a9decda122bfe03f8
정현진 <s2113@e-mirim.hs.kr>
2022-06-03 10:14:29 +0900

    연산자 오버로딩 2

--- 201 c++/04 - 01 연산자 오버로딩 (기본).cpp
+++ 201 c++/04 - 01 연산자 오버로딩 (기본).cpp
@@ -35,7 +35,7 @@ void Point::chul(void) {
 int main(void) {
 	Point p1(1, 2);
 	Point p2(3, 4);
-	Point sum = p1.operator+(p2);
+	Point sum = p1 + p2;		//p1.operator+(p2);
 	sum.chul(); //4, 6
 
 	return 0;
