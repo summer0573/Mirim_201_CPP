ddc3ee958906589fb8a19bb9f473187849c05eb4
정현진 <s2113@e-mirim.hs.kr>
2022-06-03 10:23:29 +0900

    파일 분활

--- 201 c++/04 - 01 연산자 오버로딩 (기본).cpp
+++ 201 c++/04 - 01 연산자 오버로딩 (기본).cpp
@@ -1,36 +1,6 @@
 #include <iostream>
+#include "04 - 01.h"
 using namespace std;
-
-class Point {
-private:
-	int mX;
-	int mY;
-public:
-	Point(int mX, int mY); //생성자
-	Point(void);
-
-	//연산자 오버로딩
-	Point operator+(const Point& rhs);
-
-	void chul(void);
-
-};
-
-Point::Point(int mmX, int mmY) : mX(mmX), mY(mmY) { }
-Point::Point() {}
-
-Point Point::operator+(const Point& rhs)
-{
-	Point p;
-	p.mX = mX + rhs.mX;
-	p.mY = mY + rhs.mY;
-	return p;
-}
-
-void Point::chul(void) {
-
-	cout << mX << " " << mY << endl;
-}
 
 int main(void) {
 	Point p1(1, 2);
