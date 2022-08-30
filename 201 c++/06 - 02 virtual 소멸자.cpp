a54f75a03d9216b483cc31de55b16c36fadfb655
정현진 <s2113@e-mirim.hs.kr>
2022-08-30 11:51:45 +0900

    소멸자 앞에는 무조건 virutal 키워드 넣기

--- 201 c++/06 - 02 virtual 소멸자.cpp
+++ 201 c++/06 - 02 virtual 소멸자.cpp
@@ -6,14 +6,14 @@ class Animal {
 
 public:
 	Animal() { cout << "Animal 积己" << endl; }
-	Animal() { cout << "Animal 家戈" << endl; }
+	virtual ~Animal() { cout << "Animal 家戈" << endl; }
 	void eat(void) { cout << "悼拱冈绢" << endl; }
 };
 
 class Tiger : public Animal {
 public:
 	Tiger() { cout << "Tiger 积己" << endl; }
-	Tiger() { cout << "Tiger 家戈" << endl; }
+	virtual ~Tiger() { cout << "Tiger 家戈" << endl; }
 };
 
 void main(void) {
