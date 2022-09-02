51f7f8745b27191611197392b209beda5c0d6cca
정현진 <s2113@e-mirim.hs.kr>
2022-09-02 09:06:08 +0900

    순수가상함수(추상화)

--- 201 c++/06 - 03  순수가상함수.cpp
+++ 201 c++/06 - 03  순수가상함수.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Animal {
-
 public:
 	virtual void eat(void) = 0;
 	virtual void roar(void) = 0;
@@ -13,25 +13,26 @@ private:
 	string name;
 	int sex;
 	int age;
-
 };
-
 
 class Tiger : public Animal {
 public:
-	virtual void eat(void) override { cout << "È£¶ûÀÌ ³È³È" << endl; }
-	virtual void eat(void) override { cout << "¾îÈï" << endl; }
-	virtual void eat(void) override { cout << "È£¶ûÀÌ ´ç´ç" << endl; }
+	void eat(void) override { cout << "È£¶ûÀÌ ³È³È" << endl; }
+	void roar(void) override { cout << "¾îÈï" << endl; }
+	void walk(void) override { cout << "È£¶ûÀÌ ´ç´ç" << endl; }
+
 };
 
 class Dog : public Animal {
 public:
-	virtual void eat(void) override { cout << "°­¾ÆÁö ³È³È" << endl; }
-	virtual void eat(void) override { cout << "¸Û¸Û" << endl; }
-	virtual void eat(void) override { cout << "°­¾ÆÁö ´ç´ç" << endl; }
+	void eat(void) override { cout << "°³ ÂÁÂÁ" << endl; }
+	void roar(void) override { cout << "¸Û¸Û" << endl; }
+	void walk(void) override { cout << "°³ »ç»Ó" << endl; }
 };
 
-void main(void) {
-	Animal* animal = new Tiger(); //½ÇÃ¼°¡ ¾ø´Â °´Ã¼´Â »ý¼º ºÒ°¡
+void main(void)
+{
+	//Animal* animal = new Animal();	//½ÇÃ¼°¡ ¾ø´Â °´Ã¼´Â »ý¼ººÒ°¡
+	Animal* animal = new Tiger();
 	delete animal;
 }
