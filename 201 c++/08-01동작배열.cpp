b7b49b0f2100d6c00bae26cded82a8a5e3d46906
정현진 <s2113@e-mirim.hs.kr>
2022-09-27 12:17:05 +0900

    iterator(반복자)

--- 201 c++/08-01동작배열.cpp
+++ 201 c++/08-01동작배열.cpp
@@ -28,7 +28,12 @@ void main(void)
 	// 자동으로 크기확장 후 삽입
 	i3.push_back(10);
 
-	// vector의 모든 원소 출력
+	//vector 데이터 변경
+	i3[5] = 1149;
+	i3.at(3) = 201;
+
+	// vector의 모든 원소 출력(반복자)
+	vector<int>::
 	for (int i = 0; i < i3.size(); i++)
 		cout << i3[i] << " ";
 }
---
+++ 201 c++/반복자.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>		// vector STL »ç¿ë
+
+using namespace std;
+
+void main(void) {
+	vector<int> v;
+	int input, tot = 0;
+	double avg;
+
+	for (int i = 0; i < 5; i++) {
+		cin >> input;
+		v.push_back(input);
+	}
+}
