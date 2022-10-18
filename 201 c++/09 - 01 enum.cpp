105d9180c90fda533d01a6ea4e8a4e70077f80c9
정현진 <s2113@e-mirim.hs.kr>
2022-10-18 11:10:05 +0900

    enum 적용, 가독성 증가

---
+++ 201 c++/09 - 01 enum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+enum Food {
+	KOREAN,
+	JAPANESE,
+	CHINESE
+};
+
+void main(void)
+{
+	string food_spot[3];
+	food_spot[Food::KOREAN] = "삼백집";
+	food_spot[Food::JAPANESE] = "야키토리 묵";
+	food_spot[Food::CHINESE] = "태화장";
+
+	cout << "맛집리스트" << endl;
+	int menu = 1;
+	switch (menu)
+	{
+	case Food::KOREAN:
+		cout << "한식 : ";
+		break;
+	case Food::JAPANESE:
+		cout << "일식 : ";
+		break;
+	case Food::CHINESE:
+		cout << "중식 : ";
+		break;
+	}
+	cout << food_spot[menu];
+}
